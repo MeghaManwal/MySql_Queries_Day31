5be289436c0b2ffd61d398506273b634fa07673b
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-20 18:13:45 +0530

    Added:To Add gender column to Employee_Payroll Table

--- Employee_Payroll/Payroll_Service.sql
+++ Employee_Payroll/Payroll_Service.sql
@@ -17,5 +17,8 @@ Insert into employee_payroll ( Name, Salary, Start_Date) values ( 'Jack Parker',
 								( 'Carol Tyler', '75000.0', '2020-04-05');
 Select * From employee_payroll;   
 Select salary from employee_payroll where Name = 'Alex Paul'; 
-Select Name from employee_payroll where Start_Date between Cast( '2020-03-10' as date) and date(now());                           
+Select Name from employee_payroll where Start_Date between Cast( '2020-03-10' as date) and date(now());     
+Alter table employee_payroll  add Gender varchar(1) after Name;   
+Update employee_payroll set Gender = 'M' where Name = 'Jack Parker' or Name = 'Riyan Smith' or Name = 'Nick Jonas' or Name = 'Alex Paul';   
+Update employee_payroll set Gender = 'F' where Name = 'Rachel Brown' or Name = 'Nia Patrik' or Name = 'Carol Tyler';            
                                                                 
