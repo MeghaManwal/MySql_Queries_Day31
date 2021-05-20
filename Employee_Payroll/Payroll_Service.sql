113a0bc9bd21dd2a96ac77fe6bda276ba29a4021
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-20 17:37:02 +0530

    Added:To Insert data into Employee_Payroll Table

--- Employee_Payroll/Payroll_Service.sql
+++ Employee_Payroll/Payroll_Service.sql
@@ -8,3 +8,11 @@ Salary      double Not Null,
 Start_Date  date Not Null,
 Primary key(ID) );
 Select * From employee_payroll;
+Insert into employee_payroll ( Name, Salary, Start_Date) values ( 'Jack Parker', '45000.0', '2020-01-15'),
+                                                                ( 'Riyan Smith', '50000.0', '2020-02-01'),
+								( 'Rachel Brown', '65000.0', '2020-01-01'),
+								( 'Nick Jonas', '49000.0', '2020-03-10'),
+								( 'Alex Paul', '38000.0', '2020-03-15'),
+								( 'Nia Patrik', '57000.0', '2020-03-20'),
+								( 'Carol Tyler', '75000.0', '2020-04-05');
+                                                                
